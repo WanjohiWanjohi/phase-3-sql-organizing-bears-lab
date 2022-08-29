f491a525933dd81e1058d9f0b223b7da881ea34b
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2022-08-29 12:37:41 +0300

    commit tree b6d05fcd74ebf8ae46c3ea77d2931747f2f71442

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1, "Mr. Chocolate", 30, "M", "Brown", "calm", True),
+    (2, "Rowdy", 20, "F", "Black", "Angry", True),
+     (3, "Tabitha", 16, "F", "Beige", "Calm", False),
+      (4, "Sergeant Brown", 20, "M", "Chocolate", "Gentle", True),
+       (5, "Melissa", 3, "F", "White", "Gentle", True),
+        (6, "Grinch", 20, "M", "Black", "Irritated",False),
+        (7, "Wendy", 20, "F", "Black", "Calm", True);
+ INSERT into bears (id, age, sex, color, temperament, alive) VALUES  (8, 28, "M", "Black", "Angry", True)
